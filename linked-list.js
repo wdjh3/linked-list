c2f3f323b08ee7e612d502554a2db8911dff438c
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-26 11:49:24 +0800

    feat: Add append() to LinkedList

--- linked-list.js
+++ linked-list.js
@@ -1,12 +1,46 @@
 class LinkedList {
-    constructor() {
-        this.linkedList = [];
+  constructor() {
+    this.head = null;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.nextNode) {
+        currentNode = currentNode.nextNode;
+      }
+      currentNode.nextNode = newNode;
     }
+  }
+
+//   prepend(value) {
+//     const newNode = new Node(value);
+//     if (!this.head) {
+//         this.head = newNode;
+//     } else {
+
+//     }
+//   }
 }
 
 class Node {
-    constructor() {
-        this.value = null;
-        this.nextNode = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
 }
+
+// example uses class syntax - adjust as necessary
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(JSON.stringify(list));
