683bd8e1b210c01f4c2ee909cd668facee437561
wdjh3 <198714742+wdjh3@users.noreply.github.com>
2026-03-26 12:10:32 +0800

    feat: Add head() to LinkedList

--- linked-list.js
+++ linked-list.js
@@ -1,14 +1,13 @@
 class LinkedList {
-  constructor() {
-    this.head = null;
-  }
+  #head = null;
+  constructor() {}
 
   append(value) {
     const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
+    if (!this.#head) {
+      this.#head = newNode;
     } else {
-      let currentNode = this.head;
+      let currentNode = this.#head;
       while (currentNode.nextNode) {
         currentNode = currentNode.nextNode;
       }
@@ -18,23 +17,30 @@ class LinkedList {
 
   prepend(value) {
     const newNode = new Node(value);
-    if (!this.head) {
-      this.head = newNode;
+    if (!this.#head) {
+      this.#head = newNode;
     } else {
-      newNode.nextNode = this.head;
-      this.head = newNode;
+      newNode.nextNode = this.#head;
+      this.#head = newNode;
     }
   }
 
   size() {
     let count = 0;
-    let currentNode = this.head;
+    let currentNode = this.#head;
     while (currentNode) {
-        count++
-        currentNode = currentNode.nextNode;
+      count++;
+      currentNode = currentNode.nextNode;
     }
 
     return count;
+  }
+
+  head() {
+    if (!this.#head) {
+        return undefined;
+    }
+    return this.#head;
   }
 }
 
@@ -48,13 +54,15 @@ class Node {
 // example uses class syntax - adjust as necessary
 // const list = new LinkedList();
 
+
 // list.append("dog");
+// console.log(list.head());
 // list.append("cat");
 // list.append("parrot");
 // list.prepend("hamster");
 // list.prepend("snake");
 // list.prepend("turtle");
 
-// console.log(list.size());
+// console.log(list.head());
 
-// console.log(JSON.stringify(list));
+// console.log(JSON.stringify(list.head()));
